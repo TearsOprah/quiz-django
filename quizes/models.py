03d01495e5860ba8b7c722575e66f9f701e5560e
meshok <qmeshokp@mail.ru>
2022-02-26 16:14:52 +0300

    models before migration

--- quizes/models.py
+++ quizes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+DIFF_CHOICES = (
+    ('easy', 'easy'),
+    ('medium', 'medium'),
+    ('hard', 'hard'),
+)
+
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text='Максимальное время на выполнение')
+    required_score_to_pass = models.IntegerField(help_text='Минимальный результат для прохождения')
+    difficulty = models.CharField(max_length=6, choices=DIFF_CHOICES)
+
+    def __str__(self):
+        return f'{self.name}-{self.topic}'
+
+    def get_questions(self):
+        return self.question_set.all()
